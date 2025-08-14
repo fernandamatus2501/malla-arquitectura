0c335d55b2088f6f473f24dd91273511dba5d02f
fernandamatus2501 <fernanda.matus2501@alumnos.ubiobio.cl>
2025-08-14 15:23:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -18,52 +18,57 @@ document.addEventListener('DOMContentLoaded', () => {
         { "codigo": "110041", "nombre": "Repr. Arquitectónica II", "creditos": 4, "año": 1, "semestre": 2, "prerrequisitos": ["110037"] },
         { "codigo": "110042", "nombre": "Edificación II", "creditos": 4, "año": 1, "semestre": 2, "prerrequisitos": ["110038"] },
         { "codigo": "110043", "nombre": "Estructuras I", "creditos": 4, "año": 1, "semestre": 2, "prerrequisitos": ["220163"] },
-        { "codigo": "110044", "nombre": "Fundamento de la Arquitectura", "creditos": 4, "año": 1, "semestre": 2, "prerrequisitos": [] },
+        { "codigo": "110044", "nombre": "Urbanismo I", "creditos": 4, "año": 1, "semestre": 2, "prerrequisitos": [] },
+        { "codigo": "110045", "nombre": "Fundamento de la Arquitectura", "creditos": 4, "año": 1, "semestre": 2, "prerrequisitos": [] },
         { "codigo": "340281", "nombre": "Inglés Comunicacional II", "creditos": 2, "año": 1, "semestre": 2, "prerrequisitos": ["340280"] },
-        { "codigo": "1", "nombre": "Formacion Integral Extra", "creditos": 2, "año": 1, "semestre": 2, "prerrequisitos": [] },
+        { "codigo": "1", "nombre": "Formacion Integral Extra I", "creditos": 2, "año": 1, "semestre": 2, "prerrequisitos": [] },
 
         // Año 2
-        { "codigo": "110049", "nombre": "Taller de Proyectos III", "creditos": 10, "año": 2, "semestre": 3, "prerrequisitos": ["110039"] },
-        { "codigo": "110047", "nombre": "Territorio y Ciudad", "creditos": 4, "año": 2, "semestre": 3, "prerrequisitos": ["110044"] },
+        { "codigo": "110046", "nombre": "Taller de Proyectos III", "creditos": 10, "año": 2, "semestre": 3, "prerrequisitos": ["110039"] },
+        { "codigo": "110047", "nombre": "Representación arquitectonica III", "creditos": 4, "año": 2, "semestre": 3, "prerrequisitos": ["110041"] },
         { "codigo": "110048", "nombre": "Diseño Bioclimático I", "creditos": 4, "año": 2, "semestre": 3, "prerrequisitos": [] },
+        { "codigo": "110049", "nombre": "Estructuras II", "creditos": 4, "año": 2, "semestre": 3, "prerrequisitos": ["110043"] },
         { "codigo": "110050", "nombre": "Historia de la Arquitectura II", "creditos": 4, "año": 2, "semestre": 3, "prerrequisitos": ["110040"] },
         { "codigo": "340282", "nombre": "Inglés Comunicacional III", "creditos": 2, "año": 2, "semestre": 3, "prerrequisitos": ["340281"] },
+        { "codigo": "2", "nombre": "Formacion Integral Extra II", "creditos": 2, "año": 1, "semestre": 2, "prerrequisitos": [] },
         
-        { "codigo": "110051", "nombre": "Urbanismo I", "creditos": 4, "año": 2, "semestre": 3, "prerrequisitos": ["110044"] },
-
+        { "codigo": "110051", "nombre": "Taller de Proyectos IV", "creditos": 10, "año": 2, "semestre": 4, "prerrequisitos": ["110046"] },
+        { "codigo": "110052", "nombre": "Representación arquitectonica IV", "creditos": 4, "año": 2, "semestre": 4, "prerrequisitos": [110047] },
         { "codigo": "110053", "nombre": "Edificación III", "creditos": 4, "año": 2, "semestre": 4, "prerrequisitos": ["110042"] },
-        { "codigo": "110056", "nombre": "Estructuras II", "creditos": 4, "año": 2, "semestre": 4, "prerrequisitos": ["110043"] },
-        { "codigo": "340283", "nombre": "Inglés Comunicacional IV", "creditos": 2, "año": 2, "semestre": 4, "prerrequisitos": ["340282"] },
-        { "codigo": "110054", "nombre": "Taller de Proyectos IV", "creditos": 12, "año": 2, "semestre": 4, "prerrequisitos": ["110049"] },
+        { "codigo": "110054", "nombre": "Estructuras III", "creditos": 4, "año": 2, "semestre": 4, "prerrequisitos": ["110049"] },
         { "codigo": "110055", "nombre": "Urbanismo II", "creditos": 4, "año": 2, "semestre": 4, "prerrequisitos": ["110051"] },
-        { "codigo": "110052", "nombre": "Formación Integral", "creditos": 4, "año": 2, "semestre": 4, "prerrequisitos": [] },
-
+        { "codigo": "110056", "nombre": "Fundamentos de arquitectura II", "creditos": 4, "año": 2, "semestre": 4, "prerrequisitos": ["110045"] },
+        { "codigo": "340283", "nombre": "Inglés Comunicacional IV", "creditos": 2, "año": 2, "semestre": 4, "prerrequisitos": ["340282"] },
+        
         // Año 3
-        { "codigo": "110060", "nombre": "Diseño en Madera", "creditos": 4, "año": 3, "semestre": 5, "prerrequisitos": [] },
+        { "codigo": "110057", "nombre": "Taller de Proyectos V", "creditos": 10, "año": 3, "semestre": 5, "prerrequisitos": ["110051"] },
+        { "codigo": "110058", "nombre": "Representación arquitectonica V", "creditos": 4, "año": 3, "semestre": 5, "prerrequisitos": ["110052"] },
         { "codigo": "110059", "nombre": "Edificación IV", "creditos": 4, "año": 3, "semestre": 5, "prerrequisitos": ["110053"] },
+        { "codigo": "110060", "nombre": "Diseño en Madera", "creditos": 4, "año": 3, "semestre": 5, "prerrequisitos": ["110054"] },
         { "codigo": "110061", "nombre": "Historia de la Arquitectura lll", "creditos": 4, "año": 3, "semestre": 5, "prerrequisitos": ["110050"] },
-        { "codigo": "110058", "nombre": "Taller de Proyectos V", "creditos": 12, "año": 3, "semestre": 5, "prerrequisitos": ["110054"] },
-        { "codigo": "110067", "nombre": "Taller de Investigación", "creditos": 4, "año": 3, "semestre": 5, "prerrequisitos": ["110047"] },
+        { "codigo": "3", "nombre": "Formacion Integral Extra III", "creditos": 2, "año": 1, "semestre": 2, "prerrequisitos": [] },
 
-        { "codigo": "110065", "nombre": "Diseño Bioclimático II", "creditos": 4, "año": 3, "semestre": 6, "prerrequisitos": ["110048"] },
+        { "codigo": "110062", "nombre": "Taller de proyecto VI", "creditos": 10, "año": 3, "semestre": 6, "prerrequisitos": ["110057"] },
+        { "codigo": "110063", "nombre": "Representación arquitectónica VI", "creditos": 4, "año": 3, "semestre": 6, "prerrequisitos": ["110058"] },
         { "codigo": "110064", "nombre": "Gestión de Proyectos", "creditos": 4, "año": 3, "semestre": 6, "prerrequisitos": [] },
-        { "codigo": "110063", "nombre": "Taller de Proyectos VI", "creditos": 12, "año": 3, "semestre": 6, "prerrequisitos": ["110058"] },
-        { "codigo": "110068", "nombre": "Taller de Especialidad", "creditos": 8, "año": 3, "semestre": 6, "prerrequisitos": ["110067"] },
+        { "codigo": "110065", "nombre": "Diseño Bioclimático II", "creditos": 4, "año": 3, "semestre": 6, "prerrequisitos": ["110048"] },
+        { "codigo": "110066", "nombre": "Urbanismo III", "creditos": 4, "año": 2, "semestre": 4, "prerrequisitos": ["110055"] },
+        { "codigo": "110067", "nombre": "Fundamentos de arquitectura III", "creditos": 4, "año": 2, "semestre": 4, "prerrequisitos": ["110056"] },
+        { "codigo": "4", "nombre": "Formacion Integral Extra IV", "creditos": 2, "año": 1, "semestre": 2, "prerrequisitos": [] },
 
         // Año 4
-        { "codigo": "110072", "nombre": "Metodologías de la Investigación", "creditos": 4, "año": 4, "semestre": 7, "prerrequisitos": ["110067"] },
-        { "codigo": "110070", "nombre": "Patrimonio Arq y Urbano", "creditos": 4, "año": 4, "semestre": 7, "prerrequisitos": ["110061"] },
+        { "codigo": "110068", "nombre": "Taller de Proyectos VII", "creditos": 10, "año": 4, "semestre": 7, "prerrequisitos": ["110063"] },
         { "codigo": "110069", "nombre": "Electivo de Vinculación con el Medio I", "creditos": 4, "año": 4, "semestre": 7, "prerrequisitos": [] },
-        // Asumiendo que hay otro electivo aquí, ya que el CSV muestra II en el mismo semestre
-        { "codigo": "110070-2", "nombre": "Electivo de Vinculación con el Medio II", "creditos": 4, "año": 4, "semestre": 7, "prerrequisitos": [] },
-        { "codigo": "110071", "nombre": "Taller de Proyectos VII", "creditos": 12, "año": 4, "semestre": 7, "prerrequisitos": ["110063"] },
+        { "codigo": "110070", "nombre": "Electivo de Vinculación con el Medio II", "creditos": 4, "año": 4, "semestre": 7, "prerrequisitos": [] },
+        { "codigo": "110071", "nombre": "Patrimonio Arq y Urbano", "creditos": 4, "año": 4, "semestre": 7, "prerrequisitos": ["110061"] },
+        { "codigo": "110072", "nombre": "Metodologías de la Investigación", "creditos": 4, "año": 4, "semestre": 7, "prerrequisitos": ["110067"] },
         
-        // Asumiendo que el semestre 8 es el Taller VIII y la continuación
-        { "codigo": "110073", "nombre": "Taller de Proyectos VIII", "creditos": 12, "año": 4, "semestre": 8, "prerrequisitos": ["110071"] },
+        { "codigo": "110073", "nombre": "Taller de Proyectos VIII", "creditos": 10, "año": 4, "semestre": 8, "prerrequisitos": ["110068"] },
+        { "codigo": "110074", "nombre": "Práctica profesional", "creditos": 10, "año": 4, "semestre": 8, "prerrequisitos": ["110068"] },
+        { "codigo": "110075", "nombre": "Seminario", "creditos": 10, "año": 4, "semestre": 8, "prerrequisitos": ["110072"] },
         
         // Año 5
-        { "codigo": "110074", "nombre": "Práctica Profesional", "creditos": 10, "año": 5, "semestre": 9, "prerrequisitos": ["110068"] },
-        { "codigo": "110076", "nombre": "Taller PFC", "creditos": 12, "año": 5, "semestre": 9, "prerrequisitos": ["110073"] }, // Suponiendo Taller VIII como prerrequisito
+        { "codigo": "110076", "nombre": "Taller de fundamento", "creditos": 18, "año": 5, "semestre": 9, "prerrequisitos": ["110073"] },
         { "codigo": "110077", "nombre": "Electivo de Especialidad I", "creditos": 4, "año": 5, "semestre": 9, "prerrequisitos": ["110068"] },
         { "codigo": "110078", "nombre": "Electivo de Especialidad II", "creditos": 4, "año": 5, "semestre": 9, "prerrequisitos": ["110068"] },
         { "codigo": "110079", "nombre": "Electivo de Especialidad III", "creditos": 4, "año": 5, "semestre": 9, "prerrequisitos": ["110068"] },
